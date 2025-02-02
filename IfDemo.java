dd94ed2031c3d324dbbf0b0af165b828faa26902
UdayTayade18 <udaytayde245@gmil.com>
2025-02-02 16:05:41 +0530

    Second commit

---
+++ IfDemo.java
@@ -0,0 +1,9 @@
+class IfDemo {
+        public static void main(String[] var0) {
+           byte var1 = 4;
+           if (var1 < 5) {
+              System.out.println("ais smaller");
+           }
+     
+        }
+     }
--- pattern3.java
+++ pattern3.java
@@ -1,8 +1,7 @@
 class pattern3 {
     public static void main(String[]args){
-       // int x=14;
+        int x=14;
         for(int i=1;i<=4;i++){
-            int x=14;
             for(int j=1;j<=4;j++){
                 System.out.print(" "+x);
             }
